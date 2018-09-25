9c38c4bb4e686e69b010427801d642e471e96c35
pthung <pthung>
2018-09-25 17:08:52 +0700

    https

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Routes from '../routes/routes';
 import store from '../stores/rootStore';
-
 export default function App(props) {
   return (
     <Provider store={store}>
-        <Routes />
+          <Routes />
     </Provider>
   );
 }
